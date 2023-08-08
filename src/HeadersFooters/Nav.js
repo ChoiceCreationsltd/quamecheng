5c22b06cca7d865b24722998588736054445df1c
joshua-inf <jsibanda407@gmial.com>
2023-08-08 10:54:56 +0200

    first coommit

---
+++ src/HeadersFooters/Nav.js
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom"
+import image from '../Images/qwamech logo PNG.png'
+
+const Nav = () => {
+    const changebg = () => {
+        let navbar = document.getElementById('navbar-cust');
+        let scrollValue = window.scrollY;
+
+        if(scrollValue < 150) {
+            navbar.classList.remove('navbar-cust');
+        } else {
+            navbar.classList.add('navbar-cust');
+        }
+    }
+     window.addEventListener('scroll', changebg);
+    return(
+        <>
+        <div className="navbar-custom" style={{position:'fixed', top:'0', left:'0', right:'0'}}>
+            <div className="p-3 navbar-cust" id="navbar-cust">
+                <div className="d-flex fw-bold text-white justify-content-between">
+                    <div className="nav-custom-links">
+                        <NavLink to='/Home'>
+                            <img src={image} width='150px' />
+                        </NavLink>
+                    </div>
+                    <div>
+                        <div className="nav-custom-links d-none fs-5 d-md-flex" style={{gap:'20px'}}>
+                            <NavLink to='/About'>About</NavLink>
+                            <NavLink to='/Products&Services'>Products & Services</NavLink>
+                            <NavLink to='/Contactus'>Contact us</NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                {/* <div style={{height:'5px'}} className="bg-danger"></div> */}
+        </div>
+        </>
+    )
+}
+
+export default  Nav
---
+++ src/Pages/Home.js
@@ -0,0 +1,119 @@
+import image from '../Images/image1.jpg'
+import image1 from '../Images/image1.png'
+import image2 from '../Images/gear-metal-wheels-close-up.jpg'
+import image3 from '../Images/close-up-metal-gears-3d-rendering.jpg'
+import image4 from '../Images/gears-cogwheels.jpg'
+import { BsGearWideConnected } from 'react-icons/bs'
+
+const HomePage = () => {
+    const Cont = () => {
+        return (
+            <>
+                <div className='col-md-4 p-2 col-6 text-white text-center'>
+                    <div className='d-flex justify-content-center flex-column' style={{ height: '300px', backgroundColor: 'rgba(134, 145, 145, 0.3)' }}>
+                        <div className='p-3'>
+                            <BsGearWideConnected size={90} />
+                            <div className='py-3' style={{ fontSize: '10px' }}>
+                                stuff about the above logo
+                                stuff about the above logo
+                                stuff about the above logo
+                                stuff about the above logo
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+    return (
+        <>
+            <div>
+                <div style={{ minHeight: "100vh", background: `url(${image}) no-repeat fixed  right`, backgroundSize: 'cover' }}>
+                    <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', height: '100%' }}>
+                        <div className='p-3' style={{ paddingTop: '100px'}}>
+                            <div style={{ height: '60vh' }} className='d-flex justify-content-center flex-column'>
+                                <div>
+                                    <div className=' text-center h-100'>
+                                        <div className=''>
+                                            <div className=''>
+                                                <div className='text-white' style={{}}>
+                                                    <div className='fw-bold display-5'>
+                                                        QUAMECH ENGINEERING <br /> SERVICES
+                                                    </div>
+                                                    <div className='fs-4 fw-light py-4'>
+                                                        some write up for the engineering company to identify themselves
+                                                    </div>
+                                                    <div>
+                                                        <button className='btn btn-outline-info text-white btn-lg rounded-0'>Contact us now</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='px-3 bg' style={{paddingBottom:"200px"}}>
+                            <div>
+                                <div className='row'>
+                                    <div className='col-md-1'></div>
+                                    <div className='col-12 col-md-10'>
+                                        <div>
+                                            <div className='row'>
+                                              <Cont/>
+                                              <Cont/>
+                                              <Cont/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='col-md-1'></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='' style={{overflow:'hidden'}}>
+                    <div className='row'>
+                        <div className='col-md-4 d-none d-md-block col-12' >
+                            <div>
+                                <img src={image1} width='100%'/>
+                            </div>
+                        </div>
+                        <div className='col-md-8 col-12 p-5'>
+                            <div className='row' style={{height:'100%'}}>
+                                <div className='col-md-1'></div>
+                                <div className='col-md-10 col-12' >
+                                    <div className='d-flex flex-column justify-content-center ' style={{height:'100%'}}>
+                                        <div>
+                                            <div className=''>
+                                                <div className='h3'>We are here to serve you</div>
+                                                <div className='display-3'>
+                                                    Contact us so that we can give you professional work, because we are the best in what we do
+                                                </div>
+                                                <div className='fw-bold fs-5'>
+                                                    need more convincing? no problem here are some of the services we offer
+                                                </div>
+                                                <div className='py-3'>
+                                                    <button className='btn btn-outline-danger rounded-0 btn-lg'>Check out our services</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='col-md-1'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div className='' style={{height:'300px', background:`url(${image3}) center no-repeat`, backgroundSize:'100% auto'}}></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default HomePage
